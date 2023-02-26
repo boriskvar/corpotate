caaa46b1557ea091406b66b1bd27ebc2b8c9b0cd
boriskvar <bkvartyuk@gmail.com>
2023-02-26 18:01:14 +0200

    role

--- app/User.php
+++ app/User.php
@@ -27,4 +27,9 @@ class User extends Authenticatable
     public function articles() {
 		return $this->hasMany('Corp\Article');
 	}
+
+    public function roles() {
+		return $this->belongsToMany('Corp\Role','role_user');
+	}
+
 }
